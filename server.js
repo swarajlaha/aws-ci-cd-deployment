5366fd2edc036820ab924e70eb55d55b1a700fa2
swarajlaha <swaraj.srj@gmail.com>
2021-03-08 21:01:10 +0530

    response - API Running

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => res.json('API Running'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
